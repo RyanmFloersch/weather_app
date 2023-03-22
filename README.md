9ad13ca4a06d3e0112124b543a097305f0816412
Ryan <ryanmfloersch@gmail.com>
2023-03-22 00:57:15 -0400

    GOt the history bar working. I need to have the forecast clear itself of old info before taking in new info. Also fixing display of the page.

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ A weather dashboard that will run in the browser and feature dynamically updated
 ### Resources
 
 https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon
+https://stackoverflow.com/questions/5410745/how-can-i-get-a-list-of-the-items-stored-in-html-5-local-storage-from-javascript
--- assets/js/script.js
+++ assets/js/script.js
@@ -14,30 +14,77 @@ var cityName = document.querySelector('#name');
 var temp = document.querySelector('#temp');
 var wind = document.querySelector('#wind');
 var hummid = document.querySelector('#hummid');
+var currentDate = new Date();
+var history = document.querySelector('#history');
+
+
+//A function to formate the date and to increment it if needed
+// increment must be an integer
+function getNewDate(increment){
+    var day = currentDate.getDate() + increment;
+    var newDate = currentDate.getMonth() + "/" + day + "/" + currentDate.getFullYear();
+    return newDate;
+}
 
 
 
-// var testCity = 'New York';
-// var testQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + testCity+ "&appid=" + ApiKey;
-// fetch(testQueryURL).then(function (resObj){
-//     console.log(resObj);
-//     return resObj.json();
-// }).then(function (res){
-//     console.log(res);
-// });
+function setupForecast(data){
+    console.log(data.list);
+
+   
+    fiveDayForecast = document.querySelectorAll('#fiveDayForecast > *');
+    
+    for (var i = 0; i < 5; i++) {
+        
+        console.log(i+": "+data.list[i]);
+        var day = $(fiveDayForecast[i]);
+        day.append(`<p>${getNewDate(i)}</p>`);
+        // adds an image in a paragraph using the index as a unique id
+        day.append(`<p><img id="${i}" src=""/></p>`);
+        console.log(data.list[i].weather[0].icon);
+        var iconcode = data.list[i].weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        //variable for targeting id
+        var realId = "#"+i;
+        $(realId).attr('src', iconurl);
+       
+        day.append(`<p>Temp: ${data.list[i].wind.speed} mph</p>`);
+        day.append(`<p>Humidity: ${data.list[i].main.humidity}</p>`);
+
+    }
+    
+
+
+}
 
 function displayWeather(data){
-    cityName.innerText = data.name + "(" + data.dt + ")" ;
+    cityName.innerText = data.name + "(" + getNewDate(0) + ")" ;
     var iconcode = data.weather[0].icon;
     var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
     $('#wicon').attr('src', iconurl);
     temp.innerText = "Temp: " + Math.floor( (data.main.temp-273.15)*9/5+32) + "°f";
     wind.innerText = "Wind: " + data.wind.speed + "MPH";
     hummid.innerText = "Hummidity: " + data.main.humidity +"%";
+    console.log(data);
+    console.log(data.coord);
+    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+ data.coord.lat+"&lon="+data.coord.lon+"&appid=" + ApiKey;
 
+    fetch(forecastURL).then(function (resObj){
+        return resObj.json();
+    }).then(setupForecast);
+
+    localStorage.setItem(data.name, data);
 
 }
 
+function historySearchCity(btnValue){
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + btnValue + "&appid=" + ApiKey;
+    fetch(queryURL).then(function (resObj){
+        console.log(resObj);
+        return resObj.json();
+    }).then( displayWeather);
+}
 
 function searchCity(){
     city = querriedCity.value;
@@ -50,5 +97,25 @@ function searchCity(){
     }).then( displayWeather);
 }
 
+
+function checkHistory(){
+    for (var i=0; i < localStorage.length;i++) {
+        var key = localStorage.key(i);
+        $('#history').append(`<button class="my-3 col-12 btn btn-outline-dark">${key}</button>`);
+    }
+}
+
+// Checks the local Storage for any past searches
+checkHistory();
+// Creates a list of the past search buttons added
+var historyBtns = document.querySelectorAll('#history > button');
+
+// Sets up a listener for the submit button
 submitBtn.addEventListener('click', searchCity);
 
+for (el of historyBtns) {
+    el.addEventListener('click', function (event) {
+        btnName = event.target.innerText;    
+        historySearchCity(btnName);   
+    });
+}
